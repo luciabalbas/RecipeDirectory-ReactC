4d523ece3a6f271ed235e3d09337f84da08c83e4
luciabalbas <luciabalrub@gmail.com>
2025-08-28 15:11:29 +0200

    Installing React-Route 5.1 & Creating Routes to the Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Page Components
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Search from './pages/search/Search'
+import Recipe from './pages/recipe/Recipe'
+
+// styles
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/create' component={Create} />
+          <Route path='/search' component={Search} />
+          <Route path='/recipes/:id' component={Recipe} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
