92b4cf0e71a3347a563c3023072a9304b45ce3ae
luciabalbas <luciabalrub@gmail.com>
2025-08-29 14:39:31 +0200

    Redirection to home after create a new recipe

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,4 +1,5 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import { useFetch } from '../../hooks/useFetch'
 
 // styles
@@ -11,8 +12,9 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState('')
   const [ingredients, setIngredients] = useState([])
   const ingredientInput = useRef(null)
-
+  
   const { postData, data, error } = useFetch('http://localhost:3000/recipes', 'POST') 
+  const history = useHistory()
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -29,6 +31,12 @@ export default function Create() {
     setNewIngredient('')
     ingredientInput.current.focus()
   }
+
+  useEffect(() =>{
+    if (data) {
+      history.push('/')
+    }
+  }, [data, history])
 
   return (
     <div className='create'>
