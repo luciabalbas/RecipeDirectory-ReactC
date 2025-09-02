175b4f1e186f17487d6dc0f3658e5f67b68dfb68
luciabalbas <luciabalrub@gmail.com>
2025-09-02 22:26:14 +0200

    Fetching Firestore Document by ID

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useFetch } from '../../hooks/useFetch.jsx'
 import { useParams } from 'react-router-dom'
 import { useTheme } from '../../hooks/useTheme.jsx'
+import { projectFirestore } from '../../firebase/config.js'
 
 //styles
 import './Recipe.css'
 
 export default function Recipe() {
   const { id } = useParams()
-  const url = 'http://localhost:3000/recipes/' + id
-  const { data: recipe, isPending, error } = useFetch(url)
+  // const url = 'http://localhost:3000/recipes/' + id
+  // const { data: recipe, isPending, error } = useFetch(url)
   const { mode } = useTheme()
+  const [recipe, setRecipe] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    setIsPending(true)
+
+    projectFirestore.collection('recipes').doc(id).get().then(doc => {
+        console.log(doc);
+        if (doc.exists) {
+          setIsPending(false)
+          setRecipe(doc.data())
+        } else {
+          setError('No recipe to load')
+          setIsPending(false)
+        }
+      })
+  }, [id])
 
   return (
     <div className={`recipe ${mode}`}>
@@ -21,7 +40,7 @@ export default function Recipe() {
           <h1 className='page-title'>{recipe.title}</h1>
           <p>Takes {recipe.cookingTime} to cook</p>
           <ul>
-            {recipe.ingredients.map(ing => 
+            {recipe.ingridients.map(ing => 
               <li key={ing}>{ing}</li>
             )}
           </ul>
